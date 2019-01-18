e50e9f4d5bcc8059f5aca1e3fe234a65f471b5fc
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-01-18 18:11:29 +0100

    update for Malmo

--- joint_model/_build.sh
+++ joint_model/_build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-
-R --slave --silent <<RSCRIPT
-  rmarkdown::render(input = 'joint_model.Rmd', encoding = 'UTF-8')
-RSCRIPT
+Rscript -e ="rmarkdown::render(input = 'joint_model.Rmd', encoding = 'UTF-8')"
